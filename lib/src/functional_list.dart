528da42028d0ceb29cd14fe3775e362c1c95cc04
Marco Scannadinari <m@scannadinari.co.uk>
2019-02-14 16:16:52 +0000

    Implement addAll()

--- lib/src/functional_list.dart
+++ lib/src/functional_list.dart
@@ -2,3 +2,8 @@
 List<T> add<T>(List<T> list, T value) {
   return list + <T>[value];
 }
+
+/// Returns a new list with [values] appended to [list]
+List<T> addAll<T>(List<T> list, Iterable<T> values) {
+  return list + values.toList();
+}
